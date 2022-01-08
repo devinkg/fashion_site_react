01c347120f5863b1ccbb238c8c4dda34ef4ef646
Devin Godage <devinkanilka@gmail.com>
2022-01-09 03:28:07 +0530

    material UI added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Search } from '@mui/icons-material';
+
+const Container = styled.div`
+    height: 60px;
+`
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Left = styled.div`
+   flex: 1;
+`;
+
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+    border: 1px solid lightgray;
+`;
+
+const Center = styled.div`
+   flex: 1;
+`;
+
+const Right = styled.div`
+    flex: 1;
+`;
 
 const Navbar = () => {
     return (
-        <div>
-            navbar
-        </div>
+        <Container>
+            <Wrapper>
+                <Left>
+                    <Language>EN</Language>
+                    <SearchContainer>
+                        <Search />
+                    </SearchContainer>
+                </Left>
+                <Center>center</Center>
+                <Right>Right</Right>
+            </Wrapper>
+        </Container>
     )
 }
 
