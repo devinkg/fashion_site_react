e6bd5bb8aef7bcb6e68036afe62747b576ba8494
Devin Godage <devinkanilka@gmail.com>
2022-01-23 22:55:41 +0530

    Outside resources added and data.js updated

--- src/data.js
+++ src/data.js
@@ -8,16 +8,16 @@ export const sliderItems = [
     },
     {
         id: 2,
-        img: "https://i.ibb.co/DG69bQ4/2.png",
+        img: "https://i.ibb.co/p2GkXj0/fashion-model-in-park-ccexpress.png",
         title: "AUTUMN COLLECTION",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        bg: "fcf1ed",
+        bg: "f3ebe8",
     },
     {
         id: 3,
-        img: "https://i.ibb.co/cXFnLLV/3.png",
-        title: "LOUNGEWEAR LOVE",
+        img: "https://i.ibb.co/1ms68qk/model-in-blue-dress.png",
+        title: "WINTER LOVE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        bg: "fbf0f4",
+        bg: "dce0e3",
     },
 ];
