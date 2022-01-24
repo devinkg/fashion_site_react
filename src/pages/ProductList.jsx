d648ffb05f1bc303f88c3267f2bf7d9d0e5e6e1f
Devin Godage <devinkanilka@gmail.com>
2022-01-25 01:38:38 +0530

    Product List page partially done

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,13 +1,46 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import Navbar from '../components/Navbar';
+import Announcement from '../components/Announcement';
+import Products from '../components/Products';
+import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
 
 const Container = styled.div`
 
 `;
 
+const Title = styled.h1`
+    margin:20px;
+`;
+
+const FilterContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Filter = styled.div`
+    margin: 20px;
+`;
+
+const FilterText = styled.span`
+    font-size: 20px;
+    font-weight: 600;
+`;
+
+
 const ProductList = () => {
     return (
         <Container>
-            
+            <Navbar />
+            <Announcement />
+            <Title>Dresses</Title>
+            <FilterContainer>
+                <Filter><FilterText>Filter Products:</FilterText></Filter>
+                <Filter><FilterText>Sort Products:</FilterText></Filter>
+            </FilterContainer>
+            <Products />
+            <Newsletter />
+            <Footer />
         </Container>
     )
 }
