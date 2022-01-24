8da5b90a0c4f3faab1f94caff845d01b2fb53053
Devin Godage <devinkanilka@gmail.com>
2022-01-25 01:49:26 +0530

    almost finished ProductList Page

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -25,6 +25,15 @@ const Filter = styled.div`
 const FilterText = styled.span`
     font-size: 20px;
     font-weight: 600;
+    margin-right: 20px;
+`;
+
+const Select = styled.select`
+    padding: 10px;
+    margin-right: 20px;
+`;
+const Option = styled.option`
+
 `;
 
 
@@ -35,8 +44,36 @@ const ProductList = () => {
             <Announcement />
             <Title>Dresses</Title>
             <FilterContainer>
-                <Filter><FilterText>Filter Products:</FilterText></Filter>
-                <Filter><FilterText>Sort Products:</FilterText></Filter>
+                <Filter><FilterText>Filter Products:</FilterText>
+                    <Select>
+                        <Option disabled selected>
+                            Color
+                        </Option>
+                        <Option>White</Option>
+                        <Option>Black</Option>
+                        <Option>Red</Option>
+                        <Option>Blue</Option>
+                        <Option>Yellow</Option>
+                        <Option>Green</Option>
+                    </Select>
+                    <Select>
+                        <Option disabled selected>
+                            Size
+                        </Option>
+                        <Option>XS</Option>
+                        <Option>S</Option>
+                        <Option>M</Option>
+                        <Option>L</Option>
+                        <Option>XL</Option>
+                    </Select>
+                </Filter>
+                <Filter><FilterText>Sort Products:</FilterText>
+                <Select>
+                        <Option selected>Newest</Option>
+                        <Option>Price (asc)</Option>
+                        <Option>Price (desc)</Option>
+                    </Select>
+                </Filter>
             </FilterContainer>
             <Products />
             <Newsletter />
