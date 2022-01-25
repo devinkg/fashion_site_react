a526b18da43bc0023c2c28d03ec0782ae8623bfe
Devin Godage <devinkanilka@gmail.com>
2022-01-26 02:14:53 +0530

    product page initiated and applied

---
+++ src/pages/Product.jsx
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+
+const Container = styled.div``;
+const Wrapper = styled.div`
+    padding: 50px;
+    display: flex;
+`;
+
+const ImageContainer = styled.div`
+    flex:1;
+`;
+
+const Image = styled.img`
+    width:100%;
+    height:90vh;
+    object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+    flex:1;
+    padding: 0px 50px;
+`;
+
+const Title = styled.h1`
+    font-weight: 200;
+`;
+
+const Desc = styled.p`
+    margin:20px 0px;
+`;
+
+const Price = styled.span`
+    font-weight: 100;
+    font-size: 40px;
+`;
+
+const Product = () => {
+    return (
+        <Container>
+            <Navbar />
+            <Announcement />
+            <Wrapper>
+                <ImageContainer>
+                    <Image src="https://i.ibb.co/9wq89J2/woman-wear-jumpsuit-ccexpress.png" />
+                </ImageContainer>
+                <InfoContainer>
+                    <Title>Denim Jumpsuit</Title>
+                    <Desc>Lorem ipsom dolor sit amet, consectetur adipiscing elit. Donec
+                        venenatics, dolor in finibus malesuada, lectus ipsum porta nunc, at
+                        iaculis arcu nisi sed mauris. Nulla fermentum vestibulum ex, eget
+                        tristique tortor pretium ut. Curabitur elit justo, consequat id
+                        condimentum ac, volutpat ornare.
+                    </Desc>
+                    <Price>$ 20</Price>
+                </InfoContainer>
+            </Wrapper>
+            <Newsletter />
+            <Footer />
+        </Container>
+    );
+};
+
+export default Product;
