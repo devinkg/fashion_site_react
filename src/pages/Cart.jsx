cbd9f4b50ca0d2dea7659e72ad3257cf1cd77628
Devin Godage <devinkanilka@gmail.com>
2022-01-28 00:49:35 +0530

    Cart page style changes added

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,15 +6,37 @@ import Navbar from '../components/Navbar';
 const Container = styled.div``;
 
 const Wrapper = styled.div`
-padding: 20px;
+    padding: 20px;
 `;
 
 const Title = styled.h1`
-  font-weight: 300;
-  text-align: center;
+    font-weight: 300;
+    text-align: center;
 `;
 
-const Top = styled.div``;
+const Top = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+`;
+
+const TopButton = styled.button`
+    padding: 10px;
+    font-weight: 600;
+    cursor: pointer;
+    border: ${props => props.type === "filled" && "none"};
+    background-color: ${props => props.type === "filled" ? "Black" : "transparent"};
+    color: ${props => props.type === "filled" && "white"};
+`;
+
+const TopTexts = styled.div``;
+
+const TopText = styled.span`
+text-decoration: underline;
+cursor: pointer;
+margin: 0px 10px;
+`;
 
 const Bottom = styled.div``;
 
@@ -25,7 +47,14 @@ const Cart = () => {
             <Announcement />
             <Wrapper>
                 <Title>YOUR BAG</Title>
-                <Top></Top>
+                <Top>
+                    <TopButton>CONTINUE SHOPPING</TopButton>
+                    <TopTexts>
+                        <TopText>Shopping Bag(2)</TopText>
+                        <TopText>Your Wishlist(0)</TopText>
+                    </TopTexts>
+                    <TopButton type="filled">CHECKOUT NOW</TopButton>
+                </Top>
                 <Bottom></Bottom>
             </Wrapper>
             <Footer />
