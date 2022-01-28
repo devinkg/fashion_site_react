4b865fc6ef7f13b3b7a9fb3d7b9b0262d521f3c6
Devin Godage <devinkanilka@gmail.com>
2022-01-29 01:48:03 +0530

    Cart One product UI done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,4 @@
+import { Add, Remove } from '@mui/icons-material';
 import styled from 'styled-components';
 import Announcement from '../components/Announcement';
 import Footer from '../components/Footer';
@@ -33,18 +34,85 @@ const TopButton = styled.button`
 const TopTexts = styled.div``;
 
 const TopText = styled.span`
-text-decoration: underline;
-cursor: pointer;
-margin: 0px 10px;
+    text-decoration: underline;
+    cursor: pointer;
+    margin: 0px 10px;
 `;
 
-const Bottom = styled.div``;
+const Bottom = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Info = styled.div`
+    flex:3
+`;
+
+const Product = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const ProductDetail = styled.div`
+    flex:2;
+    display: flex;
+`;
+
+const Image = styled.img`
+    width: 200px;
+`;
+
+const Details = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`;
+
+const ProductName = styled.span``;
+
+const ProductId = styled.span``;
+
+const ProductColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props => props.color};
+`;
+
+const ProductSize = styled.span``;
+
+const PriceDetail = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const ProductAmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+`;
+const ProductAmount = styled.div`
+    font-size: 24px;
+    margin: 5px;
+`;
+const ProductPrice = styled.div`
+    font-size: 30px;
+    font-weight: 200;
+`;
+
+const Summary = styled.div`
+    flex:1
+`;
 
 const Cart = () => {
     return (
         <Container>
-            <Navbar />
-            <Announcement />
+            <Navbar/>
+            <Announcement/>
             <Wrapper>
                 <Title>YOUR BAG</Title>
                 <Top>
@@ -55,9 +123,32 @@ const Cart = () => {
                     </TopTexts>
                     <TopButton type="filled">CHECKOUT NOW</TopButton>
                 </Top>
-                <Bottom></Bottom>
+                <Bottom>
+                    <Info>
+                        <Product>
+                            <ProductDetail>
+                                <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A"/>
+                                <Details>
+                                    <ProductName><b>Product:</b> JESSIE THUNDER SHOES</ProductName>
+                                    <ProductId><b>ID:</b> 9237873832938</ProductId>
+                                    <ProductColor color='black'/>
+                                    <ProductSize><b>Size:</b> 37.5</ProductSize>
+                                </Details>                            
+                            </ProductDetail>
+                            <PriceDetail>
+                            <ProductAmountContainer>
+                            <Add/>
+                            <ProductAmount>2</ProductAmount>
+                            <Remove/>
+                            </ProductAmountContainer>
+                            <ProductPrice>$ 30</ProductPrice>
+                            </PriceDetail>
+                        </Product>
+                    </Info>
+                    <Summary>Summary</Summary>
+                </Bottom>
             </Wrapper>
-            <Footer />
+            <Footer/>
         </Container>
     );
 };
