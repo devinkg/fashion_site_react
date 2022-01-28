c7e5a2aef4670262815cd73f6105022104d92bad
Devin Godage <devinkanilka@gmail.com>
2022-01-29 02:07:35 +0530

    Cart UI done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -104,15 +104,51 @@ const ProductPrice = styled.div`
     font-weight: 200;
 `;
 
-const Summary = styled.div`
-    flex:1
+const Hr = styled.hr`
+    background-color: #eeee;
+    border:none;
+    height: 1px;
 `;
+
+const Summary = styled.div`
+    flex:1;
+    border:0.5px solid lightgray;
+    border-radius: 10px;
+    padding: 20px;
+    height: 50vh;
+`;
+
+const SummaryTitle = styled.h1`
+    font-weight: 200;
+`;
+const SummaryItem = styled.div`
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+    font-weight: ${props=>props.type ==="total" && "500"};
+    font-size: ${props=>props.type ==="total" && "24px"};
+`;
+
+const SummaryItemText = styled.span`
+
+`;
+
+const SummaryItemPrice = styled.span``;
+
+const Button = styled.button`
+    width: 100%;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    font-weight: 600;
+`;
+
 
 const Cart = () => {
     return (
         <Container>
-            <Navbar/>
-            <Announcement/>
+            <Navbar />
+            <Announcement />
             <Wrapper>
                 <Title>YOUR BAG</Title>
                 <Top>
@@ -127,28 +163,67 @@ const Cart = () => {
                     <Info>
                         <Product>
                             <ProductDetail>
-                                <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A"/>
+                                <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A" />
                                 <Details>
                                     <ProductName><b>Product:</b> JESSIE THUNDER SHOES</ProductName>
                                     <ProductId><b>ID:</b> 9237873832938</ProductId>
-                                    <ProductColor color='black'/>
+                                    <ProductColor color='black' />
                                     <ProductSize><b>Size:</b> 37.5</ProductSize>
-                                </Details>                            
+                                </Details>
                             </ProductDetail>
                             <PriceDetail>
-                            <ProductAmountContainer>
-                            <Add/>
-                            <ProductAmount>2</ProductAmount>
-                            <Remove/>
-                            </ProductAmountContainer>
-                            <ProductPrice>$ 30</ProductPrice>
+                                <ProductAmountContainer>
+                                    <Add />
+                                    <ProductAmount>2</ProductAmount>
+                                    <Remove />
+                                </ProductAmountContainer>
+                                <ProductPrice>$ 30</ProductPrice>
+                            </PriceDetail>
+                        </Product>
+                        <Hr />
+                        <Product>
+                            <ProductDetail>
+                                <Image src="https://i.pinimg.com/originals/2d/af/f8/2daff8e0823e51dd752704a47d5b795c.png" />
+                                <Details>
+                                    <ProductName><b>Product:</b> HAKURA T-SHIRT</ProductName>
+                                    <ProductId><b>ID:</b> 9237873832938</ProductId>
+                                    <ProductColor color='gray' />
+                                    <ProductSize><b>Size:</b> M</ProductSize>
+                                </Details>
+                            </ProductDetail>
+                            <PriceDetail>
+                                <ProductAmountContainer>
+                                    <Add />
+                                    <ProductAmount>1</ProductAmount>
+                                    <Remove />
+                                </ProductAmountContainer>
+                                <ProductPrice>$ 20</ProductPrice>
                             </PriceDetail>
                         </Product>
                     </Info>
-                    <Summary>Summary</Summary>
+                    <Summary>
+                        <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+                        <SummaryItem>
+                            <SummaryItemText>Subtotal</SummaryItemText>
+                            <SummaryItemPrice>$ 80</SummaryItemPrice>
+                        </SummaryItem>
+                        <SummaryItem>
+                            <SummaryItemText>Estimated Shipping</SummaryItemText>
+                            <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+                        </SummaryItem>
+                        <SummaryItem>
+                            <SummaryItemText>Shipping Discount</SummaryItemText>
+                            <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+                        </SummaryItem>
+                        <SummaryItem type="total">
+                            <SummaryItemText>Total</SummaryItemText>
+                            <SummaryItemPrice>$ 80</SummaryItemPrice>
+                        </SummaryItem>
+                        <Button>CHECKOUT NOW</Button>
+                    </Summary>
                 </Bottom>
             </Wrapper>
-            <Footer/>
+            <Footer />
         </Container>
     );
 };
