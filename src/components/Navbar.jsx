b7e0a63165f22c8e980cd1466ab2bdb4ec85a40c
Devin Godage <devinkanilka@gmail.com>
2022-01-31 00:17:03 +0530

    Responsive code changes added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,7 +64,7 @@ const MenuItem = styled.div`
     font-size: 14px;
     cursor: pointer;
     margin-left: 25px;
-    ${mobile({ fontSize: "12px",marginLeft:"10px" })}
+    ${mobile({ fontSize: "12px", marginLeft: "10px" })}
 `;
 
 const Navbar = () => {
